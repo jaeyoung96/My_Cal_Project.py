83b576b94514fc29dcaa00f755fd82344266cc84
jaeyoung96 <john8567733@naver.com>
2021-06-07 00:27:56 +0900

    Initial Commit

--- Algoritmhm_Practice/Nado_Codding1.py
+++ Algoritmhm_Practice/Nado_Codding1.py
@@ -538,4 +538,126 @@ weight = round(std_weight(int(height)/100,gender),2) # 두번째 자리까지 �
 
 print("키 {0}cm {1}의 표준 체중은 {2}kg입니다.".format(height,gender,weight))
 '''
-print('a')
+'''
+import sys
+print('python', 'java', file=sys.stdout) # stdout으로 출력
+print('python', 'java', file=sys.stderr) # error 처리 가능
+print('python', 'Java', 'C++', sep=",", end='?')  # 각 문장 ","로 나눠서 출력, 문장 마무리 "?" 처리
+print('무엇이 더 재밌을까여?')
+scores = {'수학':0, '영어':50, '코딩':100}
+for subject, score in scores.items():   # scores의 key, value 값 tuple로 각각 들어감
+    print(subject,score)
+    print(subject.ljust(8), str(score).rjust(4), sep=':')
+    subject 뒤에 8칸 공간 확보 후 왼쪽 정렬, score 4칸 공간 확보 후 오른쪽 정렬하여 출력
+
+# 은행 대기순번표 (001, 002, 003)
+for num in range(1,21):
+    print('대기번호 :', str(num).zfill(3)) # 3칸 공간 확보 후 빈공간 0으로 채움
+ 
+answer = input('아무 값이나 입력 하세요 : ')
+print(type(answer))
+print('입력한 값은 :', answer, '입니다.')
+'''
+'''
+# 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print('{0: >10}'.format(500))   # 앞의 10칸 비우고 500 출력
+# 양수일 땐 +로 표시, 음수일 땐 -로 표시
+print('{0: >+10}'.format(500))
+print('{0: >+10}'.format(-500))
+# 왼쪽 정렬하고, 빈칸을 _로 채움
+print('{0:_<+10}'.format(500))  # 10칸 공간 확보 후 왼쪽 정렬하여 숫자 입력 후 남은 공간 _로 채움
+# 3자리 마다 콤마를 찍어주기
+print('{0:,}'.format(10000000000000))
+# 3자리 마다 콤마를 찍어주기, +,- 부호 붙이기
+print('{0:+,}'.format(10000000000000))
+print('{0:+,}'.format(-10000000000000))
+# 3자리 마다 콤마를 찍어주기, +,- 부호 붙이기, 자릿수 확보하기, 빈자리는 ^로 채우기
+print('{0:^<+30,}'.format(10000000000000))
+# 소수점 출력
+print('{0:f}'.format(5/3))
+# 소수점 특정 자리수 까지만 표시
+print('{0:.2f}'.format(5/3))    # 소수점 둘째자리까지
+'''
+'''
+score_file = open('score.txt', 'w', encoding='utf8')    # 파일 write모드, 한글 허용
+print('수학 : 0', file=score_file)
+print('영어 : 50', file=score_file)
+score_file.close()
+
+score_file = open('score.txt', 'a', encoding='utf8')    # 파일 덮어쓰기(append)
+score_file.write('과학 : 80')
+score_file.write('\n코딩 : 100')  # 명시적인 줄바꿈
+
+score_file = open('score.txt', 'r', encoding='utf8')    # score.txt 읽어오기
+print(score_file.read())
+score_file.close()
+
+score_file = open('score.txt', 'r', encoding='utf8')
+print(score_file.readline(), end='')    # 줄별로 읽기, 한 줄 읽고 커서 다음줄로 이동
+print(score_file.readline(), end='')    # 줄별로 읽기, 한 줄 읽고 커서 다음줄로 이동
+print(score_file.readline(), end='')    # 줄별로 읽기, 한 줄 읽고 커서 다음줄로 이동
+print(score_file.readline(), end='')    # 줄별로 읽기, 한 줄 읽고 커서 다음줄로 이동
+
+score_file = open('score.txt', 'r', encoding='utf8')
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line, end='')
+score_file.close()
+
+score_file = open('score.txt', 'r', encoding='utf8')
+lines = score_file.readlines()  # list 형태로 저장
+for line in lines:
+    print(line, end='')
+score_file.close()
+'''
+''' pickle : file에 저장하고 load를 통해 내용을 불러올 수 있는 함수
+import pickle
+# profile_file = open('profile.pickle', 'wb') # binarry file write
+# profile = {'이름':'박명수', '나이':30, '취미':['축구','골프','코딩']}
+# print(profile)
+# pickle.dump(profile, profile_file)  # profile에 있는 정보를 file에 저장
+# profile_file.close()
+
+profile_file = open('profile.pickle', 'rb') # binarry file read
+profile = pickle.load(profile_file) # file에 있는 정보를 profile에 불러오기
+print(profile)
+profile_file.close()
+'''
+''' with
+import pickle
+# profile_file.close() 생략 가능
+with open('profile.pickle', 'rb') as profile_file:  # file open 후 내용을 profile_file 변수에 저장
+    print(pickle.load(profile_file))
+
+with open('study.txt', 'w', encoding='utf8') as study_file:
+    study_file.write('파이썬을 열심히 공부하고 있어요')
+
+with open('study.txt', 'r', encoding='utf8') as study_file:
+    print(study_file.read())
+'''
+''' Quiz
+Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
+
+- x 주차 주간보고 -
+부서 :
+이름 : 
+업무 요약 :
+
+1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
+
+조건 : 파일명은 '1주차.txt', '2주차.txt',... 와 같이 만듭니다.
+
+for n in range(1,51):
+    with open(str(n) + '주차.txt', 'w', encoding='utf8') as report_file:
+        report_file.write('- {0} 주차 주간보고 -'.format(n))
+        report_file.write('\n부서 : ')
+        report_file.write('\n이름 : ')
+        report_file.write('\n업무 요약 : ')
+
+with open('5주차.txt', 'r', encoding='utf8') as report_file:
+    print(report_file.read())
+'''
+
